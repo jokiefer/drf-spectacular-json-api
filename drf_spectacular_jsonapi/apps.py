f1654ef6e17329252a8ee37a0ddd6b61e6adb084
jokiefer <jonas.kiefer@live.com>
2023-12-12 13:23:52 +0100

    Update CHANGELOG.md, __init__.py, and apps.py

--- drf_spectacular_jsonapi/apps.py
+++ drf_spectacular_jsonapi/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class DRFSpectacularJsonApiConfig(AppConfig):
     name = 'drf_spectacular_jsonapi'
     verbose_name = "drf-spectacular-jsonapi"
-
-    def ready(self):
-        import drf_spectacular_jsonapi.openapi  # noqa: F408
